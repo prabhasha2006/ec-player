8dc442e006c5c8f96ee2f709528495787c43f5a8
Evelocore <kumuthuprabhasha@gmail.com>
2026-01-25 17:39:57 +0530

    players docs

--- src/components/MediaPlayerDocs.tsx
+++ src/components/MediaPlayerDocs.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { AudioLines, Upload, CassetteTape, SquareMinus, ChevronDown, ChevronUp, Copy, Check, Settings, Info, TvMinimalPlay, Code, Sun, Moon } from 'lucide-react';
-//import { VisualizePlayer, ThemeSelector, themes, WaveAudioPlayer, NanoAudioPlayer, VideoPlayer } from 'ecplayer';
-import { VisualizePlayer, ThemeSelector, themes, WaveAudioPlayer, NanoAudioPlayer, VideoPlayer } from './Player.js';
+import { VisualizePlayer, ThemeSelector, themes, WaveAudioPlayer, NanoAudioPlayer, VideoPlayer } from 'ecplayer';
+//import { VisualizePlayer, ThemeSelector, themes, WaveAudioPlayer, NanoAudioPlayer, VideoPlayer } from './Player.js';
 
 const importFrom = 'ecplayer'
-const npmVersion = '1.0.2'
+const npmVersion = '1.0.3'
 
 const themeConfig = {
     light: {
